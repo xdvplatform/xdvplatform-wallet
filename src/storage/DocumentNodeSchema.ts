5e7618ce39bde1555894275e50fbdbdc9e32a496
Rogelio <molekilla@gmail.com>
2020-05-27 12:11:53 -0500

    indexable type

--- src/storage/DocumentNodeSchema.ts
+++ src/storage/DocumentNodeSchema.ts
@@ -2,25 +2,44 @@ import { IsArray, ArrayMaxSize, IsDefined, MaxLength, IsOptional, IsBase64, IsNu
 import { AccountNodeSchema,  } from './AccountNodeSchema';
 import { LogNodeSchema } from './LogNodeSchema';
 
+interface Indexable<T> {
+    [index: string]: T;
+}
+class DocumentItemNodeSchema{
+
+    @IsDefined()
+    @IsBase64()
+    public document: Indexable<string>;
+
+}
 export class DocumentNodeSchema {
 
     @IsDefined()
     public issuer: AccountNodeSchema;
 
     @MaxLength(100)
-    public tag: string;
-
-    @IsDefined()
-    @IsBase64()
-    public data: string;
+    public tag: Indexable<DocumentItemNodeSchema>;
 
     @IsOptional()
-    public detachedSignatures?: any;
+    public detachedSignatures?: Indexable<any>;
 
     @IsOptional()
-    public logs?: LogNodeSchema[];
+    public logs?: Indexable<LogNodeSchema>;
 
 }
+// <DocumentNodeSchema>{
+//     issuer: new AccountNodeSchema(),
+//     tag:{
+//         ['a']:{
+//             document: {
+//                '111111': 'asssaa'
+//             }
+//         }
+//     }
+// }
+
+// cid/tag/invoices/document/11111/data
+
 
 export class JWTHeader {
     typ: string;
