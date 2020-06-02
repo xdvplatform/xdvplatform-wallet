a605bd1fcdde538b0d90ac86df50b27d1d93205f
Rogelio <molekilla@gmail.com>
2020-06-02 13:00:22 -0500

    pubsub done

--- src/comm/XDVComm_spec.ts
+++ src/comm/XDVComm_spec.ts
@@ -6,8 +6,10 @@ import { DIDMethodXDV } from '../did';
 import * as hash from 'hash.js';
 let localStorage = {};
 
-const alice = new IpldClient();
-const bob = new IpldClient();
+import { Pubsub } from './Pubsub';
+
+const alice = new Pubsub();
+const bob = new Pubsub();
 
 const xdvMethodShared = new DIDMethodXDV(alice);
 
@@ -22,20 +24,9 @@ describe("#xdvcomm", function () {
 
     // init bob
     await bob.initialize();
-    const topic1 = hash.sha256().digest('hex');
-    const topic2 = hash.sha256().digest('hex');
 
-    const peerAlice = await alice.ipfsClient.id();
-    const peerBob = await bob.ipfsClient.id();
-
-    const aAddr = peerAlice.addresses
-      .find(ma => ma.nodeAddress().address === '127.0.0.1')
-    const bAddr = peerBob.addresses
-      .find(ma => ma.nodeAddress().address === '127.0.0.1')
-
-    console.log('Alice: ' + peerAlice.id, 'Bob: ' + peerBob.id);
-    const c1 = await alice.ipfsClient.swarm.connect(bAddr)
-    const c2 = await bob.ipfsClient.swarm.connect(aAddr)
+    const a = await alice.connectPeer(bob.currentAddress);
+    const b = await bob.connectPeer(alice.currentAddress);
 
     // Peers exchange:
     // - topic
@@ -43,34 +34,39 @@ describe("#xdvcomm", function () {
     // - key exchange #1 (public keys for message encryption)
     // - key exchange #2 (public keys for data on rest in ipld)
     // - share as JWT/JWM {topic, peer address,  pub-message, pub-data-on-rest }
-    await bob.ipfsClient.pubsub.subscribe(topic1, (msg) => {
-      if (msg.from !== peerBob.id) {
-        console.log(`Message from ${msg.from} to Bob: ${msg.data.toString()}`)
-        expect(msg.from).equals(peerAlice.id);
-      }
-    }, { discover: true });
-    await alice.ipfsClient.pubsub.subscribe(topic1, (msg) => {
-      if (msg.from !== peerAlice.id) {
-        console.log(`Message from ${msg.from} to Alice: ${msg.data.toString()}`);
-        expect(msg.from).equals(peerBob.id);
-      }
-    }, { discover: true });
+
+
 
     // }, 2000);
-    const { waitForPeers } = require('./waitForPeers');
-    await waitForPeers(alice.ipfsClient, topic1, [peerBob.id], 30000);
-    await waitForPeers(bob.ipfsClient, topic1, [peerAlice.id], 30000);
+    const topic = Pubsub.createTopic();
+    const topicSubscriptionAlice = await alice.subscribeToTopic(topic);
+    const topicSubscriptionBob = await bob.subscribeToTopic(topic);
 
-    // // Exchange message
-    await alice.ipfsClient.pubsub.publish(topic1, `Send me some ether`)
 
-    await bob.ipfsClient.pubsub.publish(topic1, `Sending 1 ether`)
 
-    // // // unsubcribe and close connection
-    // await bob.ipfsClient.pubsub.unsubscribe(topic1);
-    // await alice.ipfsClient.pubsub.unsubscribe(topic1);
-    // await alice.ipfsClient.swarm.disconnect(bAddr)
-    // await bob.ipfsClient.swarm.disconnect(aAddr)
+    // unsubscribe from topic
+    topicSubscriptionAlice.handler.subscribe(async message => {
+      console.log(`Message from ${message.from} to Bob: ${message.data.toString()}`)
+      expect(message.from).equals(alice.currentPeerId);
+
+      await topicSubscriptionAlice.unsubscribe();
+
+    });
+
+    topicSubscriptionBob.handler.subscribe(async message => {
+      console.log(`Message from ${message.from} to Alice: ${message.data.toString()}`);
+      expect(message.from).equals(bob.currentPeerId);
+
+      await topicSubscriptionBob.unsubscribe();
+
+    });
+
+    await topicSubscriptionAlice.waitForPeers();
+    await topicSubscriptionBob.waitForPeers();
+
+
+    await topicSubscriptionAlice.send('Send me some ether');
+    await topicSubscriptionBob.send('Sending 1 ether');
 
   });
 
