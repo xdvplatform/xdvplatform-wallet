84b21497c443ffbee21ee945b73257ffcef01307
Bradley Suira <bradleysuira@gmail.com>
2020-09-21 23:03:11 -0500

    generated build

--- src/crypto/KeyConvert.ts
+++ src/crypto/KeyConvert.ts
@@ -1,4 +1,4 @@
-import * as jose from 'node-jose';
+import { JWK } from 'node-jose';
 import bs58 from 'bs58';
 import { composePrivateKey, decomposePrivateKey } from 'crypto-key-composer';
 import { ec, eddsa } from 'elliptic';
@@ -7,281 +7,312 @@ import { LDCryptoTypes } from './LDCryptoTypes';
 import { PrivateKey } from '../did/PrivateKey';
 import { PublicKey } from '../did';
 import Rasha from 'rasha';
-const { JWK } = jose;
 import ECKey from 'ec-key';
 
 export class X509Info {
-    countryName: string;
-    stateOrProvinceName: string;
-    localityName: string;
-    organizationName: string;
-    organizationalUnitName: string;
-    commonName: string;
+	countryName: string;
+	stateOrProvinceName: string;
+	localityName: string;
+	organizationName: string;
+	organizationalUnitName: string;
+	commonName: string;
 }
 
 export class KeyConvert {
+	public static async getX509RSA(kp: any) {
+		let jwk = kp;
+		// jwk = { alg: 'RS256', ...jwk };
+		const ldSuite = {
+			publicKeyJwk: kp.toJSON()
+		};
 
-    public static async getX509RSA(kp: JWK.RSAKey) {
+		return {
+			jwk,
+			der: undefined,
+			pemAsPrivate: await (Rasha as any).export({ jwk: kp.toJSON(true) }),
+			pemAsPublic: await (Rasha as any).export({ jwk: kp.toJSON() }),
+			ldSuite
+		};
+	}
 
+	/**
+	 * Returns private keys in DER, JWK and PEM formats
+	 * @param kp Key pair
+	 * @param passphrase passphrase
+	 */
+	public static async getP256(kp: ec.KeyPair, passphrase?: string) {
+		const options = { password: '' };
+		if (passphrase && passphrase.length > 0) {
+			options.password = passphrase;
+		}
 
-        let jwk = kp;
-        // jwk = { alg: 'RS256', ...jwk };
-        const ldSuite = {
-            publicKeyJwk: kp.toJSON(),
-        }
+		// PEM
+		const composePemKey = composePrivateKey(
+			{
+				format: 'raw-pem',
+				keyAlgorithm: {
+					id: 'ec',
+					namedCurve: 'secp256r1'
+				},
+				keyData: {
+					x: kp.getPublic().getX().toArrayLike(Buffer),
+					y: kp.getPublic().getY().toArrayLike(Buffer),
+					d: kp.getPrivate().toArrayLike(Buffer)
+				}
+			},
+			options
+		);
 
-        return {
-            jwk,
-            der: undefined,
-            pemAsPrivate: await Rasha.export({ jwk: kp.toJSON(true) }),
-            pemAsPublic: await Rasha.export({ jwk: kp.toJSON() }),
-            ldSuite,
-        };
-    }
+		// DER
+		const composeDerKey = composePrivateKey(
+			{
+				format: 'raw-der',
+				keyAlgorithm: {
+					id: 'ec',
+					namedCurve: 'secp256r1'
+				},
+				keyData: {
+					x: kp.getPublic().getX().toArrayLike(Buffer),
+					y: kp.getPublic().getY().toArrayLike(Buffer),
+					d: kp.getPrivate().toArrayLike(Buffer)
+				}
+			},
+			options
+		);
 
-    /**
-     * Returns private keys in DER, JWK and PEM formats
-     * @param kp Key pair
-     * @param passphrase passphrase 
-     */
-    public static async getP256(kp: ec.KeyPair, passphrase?: string) {
-        const options = { password: '' };
-        if (passphrase && passphrase.length > 0) {
-            options.password = passphrase;
-        }
+		if (passphrase) {
+			return {
+				der: composeDerKey,
+				pem: composePemKey
+			};
+		}
+		const keys = new ECKey(composePemKey, 'pem');
+		const ldSuite = {
+			publicKeyJwk: JSON.stringify(keys.toJSON()),
+			pubBytes: () =>
+				ethers.utils.arrayify('0x' + kp.getPublic().encodeCompressed('hex')),
+			privBytes: () => kp.getPrivate().toArrayLike(Buffer)
+		};
 
-        // PEM
-        const composePemKey = composePrivateKey({
-            format: 'raw-pem',
-            keyAlgorithm: {
-                id: 'ec',
-                namedCurve: 'secp256r1',
-            },
-            keyData: {
-                x: kp.getPublic().getX().toArrayLike(Buffer),
-                y: kp.getPublic().getY().toArrayLike(Buffer),
-                d: kp.getPrivate().toArrayLike(Buffer)
-            }
-        }, options);
+		return {
+			der: composeDerKey,
+			jwk: keys.toJSON(true),
+			pem: composePemKey,
+			ldSuite
+		};
+	}
 
-        // DER
-        const composeDerKey = composePrivateKey({
-            format: 'raw-der',
-            keyAlgorithm: {
-                id: 'ec',
-                namedCurve: 'secp256r1',
-            },
-            keyData: {
-                x: kp.getPublic().getX().toArrayLike(Buffer),
-                y: kp.getPublic().getY().toArrayLike(Buffer),
-                d: kp.getPrivate().toArrayLike(Buffer)
-            }
-        }, options);
+	/**
+	 * Returns private keys in DER, JWK and PEM formats
+	 * @param kp Key pair
+	 * @param passphrase passphrase
+	 */
+	public static async getES256K(kp: ec.KeyPair, passphrase?: string) {
+		const options = { password: '' };
+		if (passphrase && passphrase.length > 0) {
+			options.password = passphrase;
+		}
 
-        if (passphrase) {
-            return {
-                der: composeDerKey,
-                pem: composePemKey,
-            }
-        }
-        const keys = new ECKey(composePemKey, 'pem');
-        const ldSuite = {
-            publicKeyJwk: JSON.stringify(keys.toJSON()),
-            pubBytes: () => ethers.utils.arrayify('0x' + kp.getPublic().encodeCompressed('hex')),
-            privBytes: () => kp.getPrivate().toArrayLike(Buffer)
-        };
+		// DER
+		const composeDerKey = composePrivateKey(
+			{
+				format: 'raw-der',
+				keyAlgorithm: {
+					id: 'ec',
+					namedCurve: 'secp256k1'
+				},
+				keyData: {
+					x: kp.getPublic().getX().toArrayLike(Buffer),
+					y: kp.getPublic().getY().toArrayLike(Buffer),
+					d: kp.getPrivate().toArrayLike(Buffer)
+				}
+			},
+			options
+		);
 
-        return {
-            der: composeDerKey,
-            jwk: keys.toJSON(true),
-            pem: composePemKey,
-            ldSuite,
-        };
-    }
+		// PEM
+		const composePemKey = composePrivateKey(
+			{
+				format: 'raw-pem',
+				keyAlgorithm: {
+					id: 'ec',
+					namedCurve: 'secp256k1'
+				},
+				keyData: {
+					x: kp.getPublic().getX().toArrayLike(Buffer),
+					y: kp.getPublic().getY().toArrayLike(Buffer),
+					d: kp.getPrivate().toArrayLike(Buffer)
+				}
+			},
+			options
+		);
 
-    /**
- * Returns private keys in DER, JWK and PEM formats
- * @param kp Key pair
- * @param passphrase passphrase 
- */
-    public static async getES256K(kp: ec.KeyPair, passphrase?: string) {
-        const options = { password: '' };
-        if (passphrase && passphrase.length > 0) {
-            options.password = passphrase;
-        }
+		if (passphrase) {
+			return {
+				der: composeDerKey,
+				pem: composePemKey,
+				ldJsonPublic: null
+			};
+		}
+		const keys = new ECKey(composePemKey, 'pem');
+		const ldSuite = {
+			publicKeyJwk: JSON.stringify(keys.toJSON()),
+			pubBytes: () =>
+				ethers.utils.arrayify('0x' + kp.getPublic().encodeCompressed('hex')),
+			privBytes: () => kp.getPrivate().toArrayLike(Buffer)
+		};
 
-        // DER
-        const composeDerKey = composePrivateKey({
-            format: 'raw-der',
-            keyAlgorithm: {
-                id: 'ec',
-                namedCurve: 'secp256k1',
-            },
-            keyData: {
-                x: kp.getPublic().getX().toArrayLike(Buffer),
-                y: kp.getPublic().getY().toArrayLike(Buffer),
-                d: kp.getPrivate().toArrayLike(Buffer)
-            }
-        }, options);
+		return {
+			der: composeDerKey,
+			jwk: keys.toJSON(true),
+			pem: composePemKey,
+			ldJsonPublic: null,
+			ldSuite
+		};
+	}
 
-        // PEM
-        const composePemKey = composePrivateKey({
-            format: 'raw-pem',
-            keyAlgorithm: {
-                id: 'ec',
-                namedCurve: 'secp256k1',
-            },
-            keyData: {
-                x: kp.getPublic().getX().toArrayLike(Buffer),
-                y: kp.getPublic().getY().toArrayLike(Buffer),
-                d: kp.getPrivate().toArrayLike(Buffer)
-            }
-        }, options);
+	public static async openEncryptedPEMtoJWK(pem: string, passphrase: string) {
+		const obj = decomposePrivateKey(pem, {
+			passphrase
+		});
+		const composePem = composePrivateKey(obj);
+		const keys = new ECKey(composePem, 'pem');
 
-        if (passphrase) {
-            return {
-                der: composeDerKey,
-                pem: composePemKey,
-                ldJsonPublic: null
-            }
-        }
-        const keys = new ECKey(composePemKey, 'pem');
-        const ldSuite = {
-            publicKeyJwk: JSON.stringify(keys.toJSON()),
-            pubBytes: () => ethers.utils.arrayify('0x' + kp.getPublic().encodeCompressed('hex')),
-            privBytes: () => kp.getPrivate().toArrayLike(Buffer)
-        };
+		return keys.toJSON(true);
+	}
+	/**
+	 * Returns private keys in DER, JWK and PEM formats
+	 * @param kp Key pair
+	 * @param passphrase passphrase
+	 */
+	public static async getEd25519(kp: eddsa.KeyPair, passphrase?: string) {
+		const options = { password: '' };
+		if (passphrase && passphrase.length > 0) {
+			options.password = passphrase;
+		}
+		// DER
+		const composeDerKey = composePrivateKey(
+			{
+				format: 'pkcs8-der',
+				keyAlgorithm: {
+					id: 'ed25519'
+				},
+				keyData: {
+					seed: kp.getSecret()
+				}
+			},
+			options
+		);
 
-        return {
-            der: composeDerKey,
-            jwk: keys.toJSON(true),
-            pem: composePemKey,
-            ldJsonPublic: null
-            ldSuite,
-        };
-    }
+		// PEM
+		const composePemKey = composePrivateKey(
+			{
+				format: 'pkcs8-pem',
+				keyAlgorithm: {
+					id: 'ed25519'
+				},
+				keyData: {
+					seed: kp.getSecret()
+				}
+			},
+			options
+		);
 
-    public static async openEncryptedPEMtoJWK(pem: string, passphrase: string) {
-        const obj = decomposePrivateKey(pem, {
-            passphrase,
-        })
-        const composePem = composePrivateKey(obj);
-        const keys = new ECKey(composePem, 'pem');
+		/// const keys = new ECKey(composePemKey, 'pem');
+		return {
+			der: composeDerKey,
+			//  jwk: keys.toJSON(),
+			pem: composePemKey,
+			ldJsonPublic: null
+		};
+	}
 
-        return keys.toJSON(true);
-    }
-    /**
-     * Returns private keys in DER, JWK and PEM formats
-     * @param kp Key pair
-     * @param passphrase passphrase 
-     */
-    public static async getEd25519(kp: eddsa.KeyPair, passphrase?: string) {
-        const options = { password: '' };
-        if (passphrase && passphrase.length > 0) {
-            options.password = passphrase;
-        }
-        // DER
-        const composeDerKey = composePrivateKey({
-            format: 'pkcs8-der',
-            keyAlgorithm: {
-                id: 'ed25519'
-            },
-            keyData: {
-                seed: kp.getSecret(),
-            }
-        }, options);
+	public static createLinkedDataJsonFormat(
+		algorithm: LDCryptoTypes,
+		key: KeyLike,
+		hasPrivate = false
+	): PublicKey | PrivateKey {
+		const id = Buffer.from(ethers.utils.randomBytes(100)).toString('base64');
+		switch (algorithm) {
+			case LDCryptoTypes.JWK:
+				return Object.assign(
+					new PublicKey(),
+					{
+						id: `did:key:${id}`,
+						type: 'JwsVerificationKey2020',
+						publicKeyJwk: key.publicJwk
+					},
+					{}
+				);
 
-        // PEM
-        const composePemKey = composePrivateKey({
-            format: 'pkcs8-pem',
-            keyAlgorithm: {
-                id: 'ed25519'
-            },
-            keyData: {
-                seed: kp.getSecret(),
-            }
-        }, options);
+			case LDCryptoTypes.RSA:
+				return Object.assign(
+					new PublicKey(),
+					{
+						id: `did:key:${id}`,
+						type: 'RsaVerificationKey2018',
+						publicKeyPem: key.publicPem
+					},
+					{}
+				);
 
-        /// const keys = new ECKey(composePemKey, 'pem');
-        return {
-            der: composeDerKey,
-            //  jwk: keys.toJSON(),
-            pem: composePemKey,
-            ldJsonPublic: null
-        };
-    }
-
-
-    public static createLinkedDataJsonFormat(algorithm: LDCryptoTypes, key: KeyLike, hasPrivate = false): PublicKey | PrivateKey {
-        const id = Buffer.from(ethers.utils.randomBytes(100)).toString('base64');
-        switch (algorithm) {
-
-            case LDCryptoTypes.JWK:
-                return Object.assign(
-                    new PublicKey(),
-                    {
-                        id: `did:key:${id}`,
-                        type: 'JwsVerificationKey2020',
-                        publicKeyJwk: key.publicJwk,
-                    }, {});
-
-            case LDCryptoTypes.RSA:
-                return Object.assign(
-                    new PublicKey(),
-                    {
-                        id: `did:key:${id}`,
-                        type: 'RsaVerificationKey2018',
-                        publicKeyPem: key.publicPem,
-                    }, {});
-
-            case LDCryptoTypes.Sepc256r1:
-                return Object.assign(
-                    new PrivateKey(),
-                    {
-                        id: `did:key:${id}`,
-                        type: 'EcdsaSecp256k1VerificationKey2019',
-                        privateKeyBase58: hasPrivate ? bs58.encode(key.privBytes()) : undefined,
-                        publicKeyBase58: bs58.encode(key.pubBytes())
-                    }, {});
-            case LDCryptoTypes.Sepc256k1:
-                return Object.assign(
-                    new PrivateKey(),
-                    {
-                        id: `did:key:${id}`,
-                        type: 'EcdsaSecp256k1VerificationKey2019',
-                        privateKeyBase58: hasPrivate ? bs58.encode(key.privBytes()) : undefined,
-                        publicKeyBase58: bs58.encode(key.pubBytes())
-                    }, {});
-            case LDCryptoTypes.Ed25519:
-                return Object.assign(
-                    new PrivateKey(),
-                    {
-                        id: `did:key:${id}`,
-                        type: 'Ed25519VerificationKey2018',
-                        privateKeyBase58: hasPrivate ? bs58.encode(key.privBytes()) : undefined,
-                        publicKeyBase58: bs58.encode(key.pubBytes())
-                    }, {});
-        }
-    }
-    /**
- * Returns private keys in JWK and PEM formats
- * @param kp Key pair
- * @param passphrase passphrase 
- */
-    public static getRSA(rsa: any) {
-        return {
-            jwk: JWK.asKey(rsa, "json"),
-            pem: JWK.asKey(rsa, 'pkcs8')
-        };
-    }
-
-
+			case LDCryptoTypes.Sepc256r1:
+				return Object.assign(
+					new PrivateKey(),
+					{
+						id: `did:key:${id}`,
+						type: 'EcdsaSecp256k1VerificationKey2019',
+						privateKeyBase58: hasPrivate
+							? bs58.encode(key.privBytes())
+							: undefined,
+						publicKeyBase58: bs58.encode(key.pubBytes())
+					},
+					{}
+				);
+			case LDCryptoTypes.Sepc256k1:
+				return Object.assign(
+					new PrivateKey(),
+					{
+						id: `did:key:${id}`,
+						type: 'EcdsaSecp256k1VerificationKey2019',
+						privateKeyBase58: hasPrivate
+							? bs58.encode(key.privBytes())
+							: undefined,
+						publicKeyBase58: bs58.encode(key.pubBytes())
+					},
+					{}
+				);
+			case LDCryptoTypes.Ed25519:
+				return Object.assign(
+					new PrivateKey(),
+					{
+						id: `did:key:${id}`,
+						type: 'Ed25519VerificationKey2018',
+						privateKeyBase58: hasPrivate
+							? bs58.encode(key.privBytes())
+							: undefined,
+						publicKeyBase58: bs58.encode(key.pubBytes())
+					},
+					{}
+				);
+		}
+	}
+	/**
+	 * Returns private keys in JWK and PEM formats
+	 * @param kp Key pair
+	 * @param passphrase passphrase
+	 */
+	public static getRSA(rsa: any) {
+		return {
+			jwk: JWK.asKey(rsa, 'json'),
+			pem: JWK.asKey(rsa, 'pkcs8')
+		};
+	}
 }
 
-
 export interface KeyLike {
-    publicPem?: string,
-    publicJwk?: JWK.Key,
-    privBytes(): Buffer | Uint8Array;
-    pubBytes(): Buffer | Uint8Array;
+	publicPem?: string;
+	publicJwk?: JWK.Key;
+	privBytes(): Buffer | Uint8Array;
+	pubBytes(): Buffer | Uint8Array;
 }
