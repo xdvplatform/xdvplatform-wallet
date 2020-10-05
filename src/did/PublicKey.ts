daeba6e100c6ab099ca0f3b794c38aae1517942e
Bradley Suira <bradleysuira@gmail.com>
2020-10-05 16:56:05 -0500

    feat: added npm properties

--- src/did/PublicKey.ts
+++ src/did/PublicKey.ts
@@ -1,24 +1,11 @@
-import { IsDefined, IsOptional, IsEthereumAddress, IsBase64, IsHexadecimal } from 'class-validator';
 export class PublicKey {
-    @IsDefined()
-    id: string;
-    @IsDefined()
-    type: string;
-    @IsDefined()
-    owner: string;
-    @IsDefined()
-    @IsEthereumAddress()
-    ethereumAddress?: string;
-    @IsBase64()
-    @IsOptional()
-    publicKeyBase64?: string;
-    @IsOptional()
-    publicKeyBase58?: string;
-    @IsHexadecimal()
-    @IsOptional()
-    publicKeyHex?: string;
-    @IsOptional()
-    publicKeyPem?: string;
-    @IsOptional()
-    publicKeyJwk?: string;
+	id: string;
+	type: string;
+	owner: string;
+	ethereumAddress?: string;
+	publicKeyBase64?: string;
+	publicKeyBase58?: string;
+	publicKeyHex?: string;
+	publicKeyPem?: string;
+	publicKeyJwk?: string;
 }
